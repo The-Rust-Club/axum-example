b4993a9aab57aec9e7bf42a04a64300f3031de69
WERDXZ <lwerdxzl@hotmail.com>
2024-10-03 12:08:33 -0700

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    axum::serve(
+        listener,
+        Router::new()
+            .nest_service("/", ServeDir::new("public/"))
+    )
+    .await
+    .unwrap();
 }
+
